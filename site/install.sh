7e64e4f0f16d74796b2d7e654ba1dbbd44a9f515
Vladimir Petrigo <taenaru@gmail.com>
2016-05-25 01:37:13 +0300

    fix some issues with files

--- site/install.sh
+++ site/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo ln -s /home/box/web/site/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s -f /home/box/web/site/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 # sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
