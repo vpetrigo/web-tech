8c2104d5818212ba667e0c1f6ad5f9c043bad22d
Vladimir Petrigo <taenaru@gmail.com>
2016-05-24 17:35:49 +0300

    init for site

---
+++ site/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo ln -s /home/box/web/site/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+# sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+# sudo /etc/init.d/gunicorn restart
