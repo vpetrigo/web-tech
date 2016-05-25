038a7bc91ed630789da5bd916f481978efccdcef
Vladimir Petrigo <taenaru@gmail.com>
2016-05-25 14:28:36 +0300

    add gunicorn start

--- site/install.sh
+++ site/install.sh
@@ -2,5 +2,6 @@
 
 sudo ln -s -f /home/box/web/site/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo gunicorn -c etc/gu_config.py hello:app
 # sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
