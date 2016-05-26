43c807f634605983cd661ffaa056400b17120a53
Vladimir Petrigo <taenaru@gmail.com>
2016-05-26 20:42:21 +0300

    clean up

--- site/ask/gunicorn.conf.py
+++ site/ask/gunicorn.conf.py
@@ -1,4 +1,14 @@
 import multiprocessing
+import os
 
 bind = "0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
+
+user = "www"
+group = "www"
+proc_name = "ask_app"
+# set umask (in Python 3.5 077 with base 0 is not valid)
+# so 0o prefix must be used
+umask = "0o77"
+
+chdir = "/home/vpetrigo/projects/web-tech/site/ask"
