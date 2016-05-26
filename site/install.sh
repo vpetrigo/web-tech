0bcbd4dd8dbc614de0c1f0677b7cdb09989b9d22
Vladimir Petrigo <taenaru@gmail.com>
2016-05-26 14:15:51 +0300

    modify install.sh

--- site/install.sh
+++ site/install.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
+# I would like to use python3 for that
+update_pip() 
+{
+    sudo pip3 install --upgrade pip setuptools
+    sudo pip install gunicorn django
+}
+
+# update current dist
+system_update()
+{
+    sudo apt-get update -q
+    sudo apt-get upgrade -yq
+}
+
+system_update
+update_pip
+
+# move our server config to the nginx's site folder
 sudo ln -s -f /home/box/web/site/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo gunicorn -c etc/gu_config.py hello:app
+sudo service nginx restart
+
+sudo gunicorn -c etc/gu_config.py hello:app &
+sudo gunicorn -c ask/gu_config.py ask.ask.wsgi &
 # sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
