d989fd9ed98569dbff325be5ab8df7cdf67d3287
Vladimir Petrigo <taenaru@gmail.com>
2016-05-27 12:57:10 +0300

    fix example_local_conf.sh based on the BSD system

--- site/examples/example_local_conf.sh
+++ site/examples/example_local_conf.sh
@@ -1,2 +1,7 @@
-NGINX_SITE_ENABLE_PATH=/etc/nginx/site-enable/default
-PROJECT_PATH=/home/box/web
+NGINX_SITE_ENABLE_PATH="/usr/local/etc/nginx/site-enabled/"
+CUR_DIR="`pwd`"
+SERVER_NGINX_CONFIG_PATH="${CUR_DIR}/etc/nginx.conf"
+HELLO_APP_RUN_SERVER_SCRIPT="${CUR_DIR}/init_scripts/bsd/hello_app"
+ASK_APP_RUN_SERVER_SCRIPT="${CUR_DIR}/init_scripts/bsd/ask_app"
+GUNICORN_APPS_PATH="/usr/local/etc/rc.d/"
+
