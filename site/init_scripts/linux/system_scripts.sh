a80b7fbd29641a30522efbe1cb880966a547c71d
Vladimir Petrigo <taenaru@gmail.com>
2016-05-27 16:02:23 +0300

    add init scripts for Linux systems
    ask_app, hello_app - start scripts for Upstart system

--- site/init_scripts/linux/system_scripts.sh
+++ site/init_scripts/linux/system_scripts.sh
@@ -5,4 +5,8 @@ system_update()
     sudo apt-get upgrade -yq
 }
 
-
+start_app_servers()
+{
+    sudo service hello_app start GUNICORN_CONF_PATH=$1
+    sudo service ask_app start GUNICORN_CONF_PATH=$1
+}
