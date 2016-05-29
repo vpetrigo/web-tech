b00b68104145d87384c979b4a7e3f0e85eb6ad2e
Vladimir Petrigo <taenaru@gmail.com>
2016-05-29 03:31:05 +0300

    remove process daemonization
    as Runit requires that controlled process should not be
    detached from standard streams

--- site/ask/gunicorn.conf.py
+++ site/ask/gunicorn.conf.py
@@ -9,8 +9,6 @@ workers = multiprocessing.cpu_count() * 2 + 1
 user = "www"
 group = "www"
 
-daemon = True
-
 proc_name = "ask_app-server"
 # set umask (in Python 3.5 077 with base 0 is not valid)
 # so 0o prefix must be used
--- site/etc/gunicorn.conf.py
+++ site/etc/gunicorn.conf.py
@@ -7,7 +7,6 @@ bind = "0.0.0.0:8080"
 workers = multiprocessing.cpu_count() * 2 + 1
 user = "www"
 group = "www"
-daemon = True
 umask = "0o77"
 proc_name = "hello_app-server"
 
