894934262852ee6e7416adeac93adf88daa6fa38
Vladimir Petrigo <taenaru@gmail.com>
2017-03-18 23:54:36 +0300

    Add Ubuntu local_conf.sh

---
+++ site/examples/ubuntu_local_conf.sh
@@ -0,0 +1,7 @@
+NGINX_SITE_ENABLE_PATH="/usr/local/nginx/conf/site-enabled/"
+CUR_DIR="`pwd`"
+SERVER_NGINX_CONFIG_PATH="${CUR_DIR}/etc/nginx.conf"
+HELLO_APP_RUN_SERVER_SCRIPT="${CUR_DIR}/init_scripts/runit/hello_app"
+ASK_APP_RUN_SERVER_SCRIPT="${CUR_DIR}/init_scripts/runit/ask_app"
+GUNICORN_APPS_PATH="/usr/local/etc/rc.d/"
+
