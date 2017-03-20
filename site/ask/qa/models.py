64f502c956866746435ef47d34059d2c8e29121b
Vladimir Petrigo <taenaru@gmail.com>
2017-03-20 23:12:45 +0300

    Update QA models

--- site/ask/qa/models.py
+++ site/ask/qa/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-from django.contrib import auth
+from django.contrib.auth.models import User as auth_models_User
 
 # Create your models here.
+class User(auth_models_User):
+    pass
+
+
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField(auto_now=True, auto_now_add=True)
+    added_at = models.DateField(auto_now_add=True)
     rating = models.IntegerField()
-    author = models.ForeignKey("User", to_field="username", on_delete=models.PROTECT)
-    likes = models.ManyToManyField("User")
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    likes = models.ManyToManyField(User, related_name="likes")
 
 
 class QuestionManager(models.Manager):
@@ -21,11 +25,7 @@ class QuestionManager(models.Manager):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField(auto_now=True, auto_now_add=True)
-    question = models.ForeignKey("Question", on_delete=models.PROTECT)
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.PROTECT)
     author = models.TextField()
-
-
-class User(auth.models.User):
-    pass
 
