3383d3e3c1ef8e5d8e9d394d6652c36ef9409fab
Vladimir Petrigo <taenaru@gmail.com>
2017-03-21 14:46:17 +0300

    Update models.py

--- site/ask/qa/models.py
+++ site/ask/qa/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User as auth_models_User
 from django.db import connection
 
+
 # Create your models here.
 class User(auth_models_User):
     pass
