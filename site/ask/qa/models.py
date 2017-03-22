55c93922cf50caa9cf6e8f32de90e60a2e35eeea
Vladimir Petrigo <taenaru@gmail.com>
2017-03-23 01:25:01 +0300

    Clean up QA Models

--- site/ask/qa/models.py
+++ site/ask/qa/models.py
@@ -4,22 +4,6 @@ from django.db import connection
 
 
 # Create your models here.
-class User(auth_models_User):
-    pass
-
-
-class Question(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateField(auto_now_add=True)
-    rating = models.IntegerField()
-    author = models.ForeignKey(User, on_delete=models.PROTECT)
-    likes = models.ManyToManyField(User, related_name="likes")
-
-    class Meta:
-        ordering = ["-rating"]
-
-
 class QuestionManager(models.Manager):
     def new(self):
         cur = connection.cursor()
@@ -36,9 +20,21 @@ class QuestionManager(models.Manager):
         return cur.fetchall()
 
 
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(auth_models_User, on_delete=models.PROTECT)
+    likes = models.ManyToManyField(auth_models_User, related_name="likes")
+
+    class Meta:
+        ordering = ["-rating"]
+
+
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.PROTECT)
-    author = models.TextField()
+    author = models.ForeignKey(auth_models_User, on_delete=models.PROTECT)
 
