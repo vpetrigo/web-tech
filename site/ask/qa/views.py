296b7366ac3a3cb1482ec033869d946819464b1d
Vladimir Petrigo <taenaru@gmail.com>
2017-03-26 20:28:36 +0300

    Add main page of Question app view

--- site/ask/qa/views.py
+++ site/ask/qa/views.py
@@ -1,6 +1,28 @@
+from django.http import HttpResponse, HttpRequest
+from django.core.paginator import Paginator
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Question
+
 
 # Create your views here.
 def test(request, *args, **kwargs):
     return HttpResponse("OK")
+
+
+_QUESTION_ON_PAGE_LIMIT = 10
+_FIRST_PAGE = 1
+
+
+def page(request: HttpRequest):
+    page_num = request.GET.get("page", _FIRST_PAGE)
+    new_questions = Question.objects.new()
+    paginator = Paginator(new_questions, _QUESTION_ON_PAGE_LIMIT)
+
+    if page_num > paginator.num_pages:
+        page_num = paginator.num_pages
+
+    return render(
+        request,
+        "qa/base.html",
+        context={"questions": paginator.page(page_num),
+                 "page_num": page_num})
