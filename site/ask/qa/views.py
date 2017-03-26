11add427a5f6679dba371aec2ea6227c40f90c9d
Vladimir Petrigo <taenaru@gmail.com>
2017-03-26 23:05:02 +0300

    Update base.html template

--- site/ask/qa/views.py
+++ site/ask/qa/views.py
@@ -13,16 +13,35 @@ _QUESTION_ON_PAGE_LIMIT = 10
 _FIRST_PAGE = 1
 
 
-def main_page(request: HttpRequest):
-    page_num = int(request.GET.get("page", _FIRST_PAGE))
-    new_questions = Question.objects.new()
-    paginator = Paginator(new_questions, _QUESTION_ON_PAGE_LIMIT)
+def _paginate_content(request: HttpRequest, content_getter):
+    try:
+        page_num = int(request.GET.get("page", _FIRST_PAGE))
+    except ValueError:
+        page_num = _FIRST_PAGE
+
+    content = content_getter()
+    paginator = Paginator(content, _QUESTION_ON_PAGE_LIMIT)
 
     if page_num < 1:
-        raise Http404("Page not found")
-
-    if page_num > paginator.num_pages:
+        page_num = _FIRST_PAGE
+    elif page_num > paginator.num_pages:
         page_num = paginator.num_pages
+
+    return paginator, page_num
+
+
+def main_page(request: HttpRequest):
+    paginator, page_num = _paginate_content(request, Question.objects.new)
+
+    return render(
+        request,
+        "qa/base.html",
+        context={"questions": paginator.page(page_num),
+                 "page_num": page_num})
+
+
+def popular_page(request: HttpRequest):
+    paginator, page_num = _paginate_content(request, Question.objects.popular)
 
     return render(
         request,
