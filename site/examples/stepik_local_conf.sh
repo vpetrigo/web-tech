f8eed9f785636be85001f0df0b637b784ae61e03
Vladimir Petrigo <taenaru@gmail.com>
2017-04-10 20:10:32 +0300

    Add Stepik local_conf.sh

---
+++ site/examples/stepik_local_conf.sh
@@ -0,0 +1,5 @@
+NGINX_SITE_ENABLE_PATH="/etc/nginx/sites-enabled/"
+CUR_DIR="`pwd`"
+SERVER_NGINX_CONFIG_PATH="${CUR_DIR}/etc/nginx.conf"
+HELLO_APP_RUN_SERVER_SCRIPT="${CUR_DIR}/init_scripts/runit/hello_app"
+ASK_APP_RUN_SERVER_SCRIPT="${CUR_DIR}/init_scripts/runit/ask_app"
