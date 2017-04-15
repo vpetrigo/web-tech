38591d8d5c71699849479fbbb4565f3bf04ace87
Vladimir Petrigo <taenaru@gmail.com>
2017-04-16 02:32:41 +0300

    Refactor index/popular view to use Django's generics

--- site/ask/qa/urls.py
+++ site/ask/qa/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = "qa"
 
 urlpatterns = [
-    url("^$", views.main_page, name="index"),
+    url("^$", views.IndexView.as_view(), name="index"),
     url("^login/$", views.test, name="login"),
     url("^signup/$", views.test, name="signup"),
     url("^question/(?P<question_id>\d+)/$", views.question, name="question"),
     url("^ask/$", views.ask, name="ask"),
-    url("^popular/$", views.popular_page, name="popular"),
+    url("^popular/$", views.PopularView.as_view(), name="popular"),
     url("^new/$", views.test, name="new"),
 ]
--- site/ask/qa/views.py
+++ site/ask/qa/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse, HttpRequest, Http404, HttpResponseRedirect
-from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from .models import Question, Answer
 from django.contrib.auth.models import User as auth_models_User
+from django.views import generic
+from .models import Question, Answer
 from .forms import AskForm, AnswerForm
 
 
@@ -12,45 +12,19 @@ def test(request, *args, **kwargs):
     return HttpResponse("OK")
 
 
-_QUESTION_ON_PAGE_LIMIT = 10
-_FIRST_PAGE = 1
+class IndexView(generic.ListView):
+    model = Question
+    template_name = "qa/base.html"
+    context_object_name = "question_list"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Question.objects.order_by("-added_at")
 
 
-def _paginate_content(request: HttpRequest, content_getter):
-    try:
-        page_num = int(request.GET.get("page", _FIRST_PAGE))
-    except ValueError:
-        page_num = _FIRST_PAGE
-
-    content = content_getter()
-    paginator = Paginator(content, _QUESTION_ON_PAGE_LIMIT)
-
-    if page_num < 1:
-        page_num = _FIRST_PAGE
-    elif page_num > paginator.num_pages:
-        page_num = paginator.num_pages
-
-    return paginator, page_num
-
-
-def main_page(request: HttpRequest):
-    paginator, page_num = _paginate_content(request, Question.objects.new)
-
-    return render(
-        request,
-        "qa/base.html",
-        context={"questions": paginator.page(page_num),
-                 "page_num": page_num})
-
-
-def popular_page(request: HttpRequest):
-    paginator, page_num = _paginate_content(request, Question.objects.popular)
-
-    return render(
-        request,
-        "qa/base.html",
-        context={"questions": paginator.page(page_num),
-                 "page_num": page_num})
+class PopularView(IndexView):
+    def get_queryset(self):
+        return Question.objects.order_by("-rating", "-added_at")
 
 
 def question(request: HttpRequest, question_id: int):
