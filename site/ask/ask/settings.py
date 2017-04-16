1a0a5d998c48db8779ded0aef9bb0c3fcb092bb5
Vladimir Petrigo <taenaru@gmail.com>
2017-04-17 02:09:43 +0300

    Fix issue with the AUTHENTICATION_BACKENDS setting

--- site/ask/ask/settings.py
+++ site/ask/ask/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTHENTICATION_BACKENDS = (
+AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend'
-)
+]
 
 
 # Internationalization
