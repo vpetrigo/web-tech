d4cd42b2b001c7836d458bc7c18ba70d583becc9
Vladimir Petrigo <taenaru@gmail.com>
2017-04-19 21:29:51 +0300

    Update setup_db.sh

--- site/setup_db.sh
+++ site/setup_db.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+# provide this script with:
+# DB_NAME - database name to create
+# DB_USER - user who will be granted with all privileges to ${DB_NAME} database
+# DB_USER_PASSWD - new user's password
+
+check_mysql_root() {
+    # check whether it is possible to access MySQL
+    # without password ... and with root :)
+    mysql -u root &> /dev/null
+    
+    # yes, we definitely can
+    [[ `echo $?` -eq 0 ]] && return 0
+    
+    return 1
+}
 
 service mysql status &> /dev/null
 
@@ -12,11 +27,16 @@ if [[ $? -ne 0 ]]; then
         exit $?
     fi
 fi
+
+if [[ -z check_mysql_root ]]; then
+    MYSQL_CMD="mysql -u root"
+else
+    MYSQL_CMD="mysql -u root -p"
+fi
 # create DB and DB_User
 # variables DB_NAME DB_USER and DB_USER_PASSWD must be set
-mysql -u root -p -e "create database if not exists ${DB_NAME};"
-mysql -u root -p -e "create user if not exists '${DB_USER}'@'%' identified by '${DB_USER_PASSWD}';"
-mysql -u root -p -e "grant all privileges on ${DB_NAME}.* to '${DB_USER}'@'%';"
+$MYSQL_CMD -e "create database if not exists ${DB_NAME};"
+$MYSQL_CMD -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWD}' WITH GRANT OPTION";
 
-echo -e "[client]\ndatabase = ${DB_NAME}\n" > ask/ask/app.cnf
+echo -e "[client]\ndatabase = ${DB_NAME}" > ask/ask/app.cnf
 echo -e "user = ${DB_USER}\npassword = ${DB_USER_PASSWD}" >> ask/ask/app.cnf
