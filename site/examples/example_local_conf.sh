2c003c3ea42bba1aae174df5a61736c5c8d8a864
Vladimir Petrigo <taenaru@gmail.com>
2017-05-03 19:49:41 +0300

    Update example_local_conf.sh

--- site/examples/example_local_conf.sh
+++ site/examples/example_local_conf.sh
@@ -4,4 +4,3 @@ SERVER_NGINX_CONFIG_PATH="${CUR_DIR}/etc/nginx.conf"
 HELLO_APP_RUN_SERVER_SCRIPT="${CUR_DIR}/init_scripts/bsd/hello_app"
 ASK_APP_RUN_SERVER_SCRIPT="${CUR_DIR}/init_scripts/bsd/ask_app"
 GUNICORN_APPS_PATH="/usr/local/etc/rc.d/"
-
